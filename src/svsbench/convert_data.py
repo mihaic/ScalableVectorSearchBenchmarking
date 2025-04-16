6b72eac50c89455772bd8ffef5b95364d5b50c50
Mihai Capotă <mihai@mihaic.ro>
2025-04-15 17:54:15 -0700

    Add output_dir

--- src/svsbench/convert_data.py
+++ src/svsbench/convert_data.py
@@ -8,17 +8,18 @@ import typer
 from . import consts
 
 
-def convert_hdf5(input_path: Path):
+def convert_hdf5(input_path: Path, output_dir: Path):
     with h5py.File(input_path) as file:
+        name_prefix = output_dir / input_path.stem
         test = np.array(file["test"])
         svs.write_vecs(
             test,
-            f"{input_path.name}_test{consts.DTYPE_TO_SUFFIX[test.dtype.type]}",
+            f"{name_prefix}_test{consts.DTYPE_TO_SUFFIX[test.dtype.type]}",
         )
         train = np.array(file["train"])
         svs.write_vecs(
             train,
-            f"{input_path.name}_train{consts.DTYPE_TO_SUFFIX[train.dtype.type]}",
+            f"{name_prefix}_train{consts.DTYPE_TO_SUFFIX[train.dtype.type]}",
         )
         neighbors = np.array(file["neighbors"])
         if np.any(neighbors < 0):
@@ -26,14 +27,15 @@ def convert_hdf5(input_path: Path):
         if neighbors.dtype.type is not np.int32:
             raise ValueError("Neighbors dataset is not int32")
         svs.write_vecs(
-            neighbors.astype(np.uint32), f"{input_path.name}_neighbors.ivecs"
+            neighbors.astype(np.uint32),
+            f"{name_prefix}_neighbors.ivecs",
         )
 
 
-def main(input_path: Path):
+def main(input_path: Path, output_dir: Path = Path(".")):
     match suffix := input_path.suffix:
         case ".hdf5":
-            convert_hdf5(input_path)
+            convert_hdf5(input_path, output_dir)
         case _:
             raise ValueError(f"Unsupported suffix: {suffix}")
 
