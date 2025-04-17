17e2ddfa1ce4dd80d75f85bf53aac4f2fc6ff5a9
Mihai Capotă <mihai@mihaic.ro>
2025-04-17 16:30:07 -0700

    Add int8

--- src/svsbench/consts.py
+++ src/svsbench/consts.py
@@ -27,16 +27,19 @@ SUFFIX_TO_DATA_TYPE: Final = {
     ".fvecs": svs.DataType.float32,
     ".hvecs": svs.DataType.float16,
     ".ivecs": svs.DataType.uint32,
+    ".int8vecs": svs.DataType.int8,
 }
 
 SUFFIX_TO_SVS_TYPE: Final = {
     ".hvecs": "float16",
     ".fvecs": "float32",
+    ".int8vecs": "int8",
 }
 
 SVS_TYPES: Final = (
     "float16",
     "float32",
+    "int8",
     "leanvec4x4",
     "leanvec4x8",
     "leanvec8x8",
@@ -48,11 +51,13 @@ SVS_TYPES: Final = (
 SVS_TYPE_TO_DTYPE: Final = {
     "float16": np.float16,
     "float32": np.float32,
+    "int8": np.int8,
 }
 
 STR_TO_DATA_TYPE: Final = {
     "float16": svs.DataType.float16,
     "float32": svs.DataType.float32,
+    "int8": svs.DataType.int8,
 }
 
 STR_TO_LVQ_STRATEGY: Final[dict[str, svs.LVQStrategy]] = {
