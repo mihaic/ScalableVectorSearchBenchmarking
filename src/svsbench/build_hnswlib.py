da0df62e60b53a60acdc801875f276e1c20b406d
Mihai Capotă <mihai@mihaic.ro>
2026-01-15 13:54:33 -0800

    Add batch size for building

--- src/svsbench/build_hnswlib.py
+++ src/svsbench/build_hnswlib.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 import hnswlib
 import numpy as np
+from tqdm import tqdm
 
 from . import merge
 
@@ -26,7 +27,7 @@ def fvecs_read(fname):
 
 def _read_args(argv: list[str] | None = None) -> argparse.Namespace:
     """Read command line arguments."""
-    parser = argparse.ArgumentParser(description=__file__.__doc__)
+    parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("--vecs_file", help="Vectors *vecs file", type=Path)
     parser.add_argument(
         "--out_dir",
@@ -44,6 +45,9 @@ def _read_args(argv: list[str] | None = None) -> argparse.Namespace:
     parser.add_argument("--ef_construction", type=int, default=500)
     parser.add_argument("-m", type=int, default=64)
     parser.add_argument("--num_vectors", type=int)
+    parser.add_argument(
+        "--batch_size", help="Batch size", default=0, type=int
+    )
     return parser.parse_args(argv)
 
 
@@ -58,9 +62,10 @@ def main() -> None:
         ef_construction=args.ef_construction,
         m=args.m,
         num_vectors=args.num_vectors,
+        batch_size=args.batch_size,
     )
 
-def build(*, vecs_path: Path, out_path: Path, num_threads: int, distance: str, ef_construction: int, m: int, num_vectors: int | None) -> None:
+def build(*, vecs_path: Path, out_path: Path, num_threads: int, distance: str, ef_construction: int, m: int, num_vectors: int | None, batch_size: int = 0) -> None:
     X_db = merge.read_vecs(vecs_path)
     if num_vectors is not None:
         X_db = X_db[:num_vectors]
@@ -68,15 +73,23 @@ def build(*, vecs_path: Path, out_path: Path, num_threads: int, distance: str, e
         num_vectors = 0
 
     num_elements = X_db.shape[0]
+    if batch_size == 0:
+        batch_size = num_elements
+    num_batches = int(np.ceil(num_elements / batch_size))
     dim = X_db.shape[1]
     ids = np.arange(num_elements)
 
     p = hnswlib.Index(space = distance, dim = dim)
     p.init_index(max_elements = num_elements, ef_construction = ef_construction, M = m)
     p.set_num_threads(num_threads)
-    p.add_items(X_db, ids)
-    p.save_index(f"{out_path}/{Path(vecs_path).stem}-{num_vectors}_M{m}_{ef_construction}_hnswlib.bin")
-    print(f"{out_path}/{Path(vecs_path).stem}-{num_vectors}_M{m}_{ef_construction}_hnswlib.bin")
+    print(f"{batch_size=}, {num_batches=}")
+    for batch_idx in tqdm(range(num_batches)):
+        init_batch = batch_idx * batch_size
+        end_batch = min(init_batch + batch_size, num_elements)
+        p.add_items(X_db[init_batch:end_batch], ids[init_batch:end_batch])
+    idx_path = f"{out_path}/{Path(vecs_path).stem}-{num_vectors}_M{m}_{ef_construction}_B{batch_size}_hnswlib.bin"
+    p.save_index(idx_path)
+    print(idx_path)
 
 
 if __name__ == "__main__":
